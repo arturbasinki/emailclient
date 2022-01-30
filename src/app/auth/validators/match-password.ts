28c5f6c28059a62413a4c3e6eb4258d92ecc0265
airbass <artur.basinski@gmail.com>
2022-01-30 23:36:12 +0100

    Adding Class UniqueUsername

--- src/app/auth/validators/match-password.ts
+++ src/app/auth/validators/match-password.ts
@@ -1,2 +1,15 @@
-export class MatchPassword {
+import { Injectable } from '@angular/core';
+import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
+
+@Injectable({ providedIn: 'root' })
+export class MatchPassword implements Validator {
+  validate(formGroup: AbstractControl): ValidationErrors | null {
+    const { password, passwordConfirmation } = formGroup.value;
+
+    if (password === passwordConfirmation) {
+      return null;
+    } else {
+      return { passwordDontMatch: true };
+    }
+  }
 }
