55d91eb73708193bd3cd74f38db8dae6669a3887
airbass <artur.basinski@gmail.com>
2022-01-31 01:49:44 +0100

    Adding full Custom Validators Functionality

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,2 +1,40 @@
-export class UniqueUsername {
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidator,
+  ValidationErrors,
+} from '@angular/forms';
+import { catchError, map, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UniqueUsername implements AsyncValidator {
+  constructor(private http: HttpClient) {}
+
+  validate = (
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
+    const { value } = control;
+
+    return this.http
+      .post<any>('https://api.angular-email.com/auth/username', {
+        username: value,
+      })
+      .pipe(
+        map((value) => {
+          console.log(value);
+          return null;
+        }),
+        catchError((err) => {
+          console.log(err);
+          if (err.error.username) {
+            return of({ nonUniqueUsername: true });
+          } else {
+            return of({ noConnection: true });
+          }
+        })
+      );
+  };
 }
