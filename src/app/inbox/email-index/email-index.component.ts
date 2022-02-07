99600aaf050f5387ea28cc1ace370a4e56b838a9
airbass <artur.basinski@gmail.com>
2022-02-07 23:52:31 +0100

    Adding Component Placeholder for EmailShow

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailService } from '../email.service';
+
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 
 @Component({
   selector: 'app-email-index',
   templateUrl: './email-index.component.html',
-  styleUrls: ['./email-index.component.css']
+  styleUrls: ['./email-index.component.css'],
 })
 export class EmailIndexComponent implements OnInit {
+  emails: EmailSummary[] = [];
 
-  constructor() { }
+  constructor(private emailService: EmailService) {}
 
   ngOnInit(): void {
+    this.emailService.getEmails().subscribe((emails) => {
+      this.emails = emails;
+    });
   }
-
 }
