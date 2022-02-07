4b7f1848666571dbda3dcf40b0f2ca6ec049ca38
airbass <artur.basinski@gmail.com>
2022-02-08 00:28:22 +0100

    Formating Placeholder Component in His Template

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmailShowComponent } from './email-show/email-show.component';
 import { HomeComponent } from './home/home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 const routes: Routes = [
   {
     path: '', //heer we cant add 'Inbox' because it add with path from main app.module!!!!
     component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: PlaceholderComponent,
+      },
+      {
+        path: ':id',
+        component: EmailShowComponent,
+      },
+    ],
   },
 ];
 
