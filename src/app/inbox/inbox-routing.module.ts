c7142d4d24fc3ad59c95903f45f83a33a6e77c0f
airbass <artur.basinski@gmail.com>
2022-02-09 22:53:58 +0100

    Adding Shared Component Modal

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmailResolverService } from './email-resolver.service';
 import { EmailShowComponent } from './email-show/email-show.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 const routes: Routes = [
@@ -10,6 +11,10 @@ const routes: Routes = [
     path: '', //heer we cant add 'Inbox' because it add with path from main app.module!!!!
     component: HomeComponent,
     children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent,
+      },
       {
         path: '',
         component: PlaceholderComponent,
